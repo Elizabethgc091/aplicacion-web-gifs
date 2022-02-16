67757cf120b2ecb2025a9446efa491a7d0ddc13e
Elizabeth Gonzalez Cruz <elizabeth.gc091@gmail.com>
2022-02-16 17:46:52 -0600

    resolve footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 /** Style */
 import "./footer.css";
-import logogithub from "./logogithub.png";
+import linkedin from "./linkedin.svg";
 
 export default function Footer() {
   return (
     <div className="container-footer">
-      <div className="txt-gifos">
-        <p>GIFOS</p>
-        <p>ACAMICA</p>
+      <div className="copyright">
+        <p>© 2022 - GIFOS</p>
+        <p>ACAMICA</p>{" "}
       </div>
-      <div className="container-info">
+
+      <div className="info-creator">
         <p>Elizabeth González</p>
-        <a href="https://github.com/Elizabethgc091" target="_blank">
-          <img src="https://img.icons8.com/glyph-neue/64/000000/github.png" />
-        </a>
-        <a href="https://www.linkedin.com/in/elizabethgc091/" target="_blank">
-          <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" />
-        </a>
+        <div className="social-netwoks">
+          <div className="github">
+            <a href="https://github.com/Elizabethgc091" target="_blank">
+              <img src="https://img.icons8.com/glyph-neue/64/000000/github.png" />
+            </a>
+          </div>
+          <div className="linkedin">
+            <a
+              href="https://www.linkedin.com/in/elizabethgc091"
+              target="_blank"
+            >
+              <img src={linkedin} />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
